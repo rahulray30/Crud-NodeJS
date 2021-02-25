3ff43b1b8bedfe8c43b67a948c4a034197a6a034
rahul ray <rayrahul.2011@gmail.com>
2021-02-25 16:27:06 +0530

    Changes

--- routes/students.js
+++ routes/students.js
@@ -28,48 +28,45 @@ router.get("/:id", async (req, res) => {
 
 router.post("/", async (req, res) => {
 
-    console.log("inside post");
 
     try {
         console.log("inside post...");
-        let stu =  new student({
+        let stu = new student({
             name: req.body.name,
             rollNo: req.body.rollNo,
             address: req.body.address
-    
+
         })
-       let s1 = await stu.save();
-       res.json(s1);
-       console.log("inside post 2....");
-       
+        let s1 = await stu.save();
+        res.json(s1);
 
     } catch (err) {
         res.send(err);
     }
-    //console.log(s1);
 
 })
 
-router.patch("/:id" , async(req,res) => {
-        try{
-            const st = await student.findById(req.params.id);
-            st.address = req.body.address;
-            const s2 = await st.save();
-            res.json(s2);
-        } catch(err){
-            res.send(err);
-        }
+router.patch("/:id", async (req, res) => {
+    try {
+        const st = await student.findById(req.params.id);
+        st.address = req.body.address;
+        const s2 = await st.save();
+        res.json(s2);
+    } catch (err) {
+        res.send(err);
+    }
 })
 
-router.delete("/:id", async(req,res) =>{
-    try{
+router.delete("/:id", async (req, res) => {
+    try {
         const st = await student.findById(req.params.id);
         st.remove();
-        res.send("Deleted");
-        res.send(student);
+        const students = await student.find();
+        //res.send("Deleted");
+        res.json(students);
 
-    } catch(err){
-            res.send(err);
+    } catch (err) {
+        res.send(err);
     }
 })
 
